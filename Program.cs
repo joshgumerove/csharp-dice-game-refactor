d5798aad5b591d5a05f706938d6789f1be75265d
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-08-09 15:21:53 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿var random = new Random();
+var diceRollResult = random.Next(1, 7);
+
+Console.WriteLine($"What is value {diceRollResult}");
+Console.ReadKey();
+
+class Dice
+{
+    private Random _random;
+}
